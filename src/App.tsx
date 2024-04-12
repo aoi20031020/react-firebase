ade3de3a2059bb7581381b1846f3340206c09876
aoi20031020 <aoi20031020@gmail.com>
2024-04-12 18:00:03 +0900

    フロントエンド作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import { AppRouter } from "./components/AppRouter";
+import { Box, Flex, useDisclosure } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Box, Flex } from "@chakra-ui/react";
-import { SideMenu } from "./components/SideMenu";
+import DrawerMenu from "./components/DrawerMenu";
+
 const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Flex direction="column" minHeight="100vh">
-      <Box flexGrow={1}>
-        <Header />
+      <Box>
+        <Header onOpen={onOpen} />
       </Box>
-      <Box
-        w="100vw"
-        h={{ base: "calc(100vh - 80px)", lg: "calc(100vh - 100px)" }}
-      >
-        <SideMenu width="20vw" />
-        <Box
-          w={{ base: "100vw", lg: "80vw" }}
-          ml={{ base: "0vw", lg: "19vw" }}
-          p={{ base: "3", lg: "4" }}
-        >
+      <Flex>
+        <DrawerMenu isOpen={isOpen} onClose={onClose} />
+        <Box>
           <AppRouter />
         </Box>
-      </Box>
-      <Box flexGrow={1}>
+      </Flex>
+      <Box>
         <Footer />
       </Box>
     </Flex>
